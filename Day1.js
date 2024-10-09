09a4127b89d8154bcce9e8ba6c67afbe6bf317b4
gowtham jujaru <109477464+Gowthamjujaru@users.noreply.github.com>
2024-10-09 12:16:40 +0530

    Day-1 of revision

---
+++ Day1.js
@@ -0,0 +1,29 @@
+// welcome to javascript lets understand hot to print 
+console.log("welcome to javascript");
+
+// we use // to commment the unwanted code or text. 
+
+// we use connsole.log to print 
+
+// var let const and variables
+
+let a = 10 ;
+console.log("the value of a: ",a)
+let num = 20
+console.log("the sum of a and num : ",a+num)
+
+
+
+
+// General Rules for Declaring Variables:
+
+// Variable Naming:
+// Variable names can include letters, digits, underscores (_), and dollar signs ($) .
+// Names must begin with a letter, underscore, or dollar sign. They cannot start with a digit.
+// JavaScript variable names are case-sensitive (myVar and myvar are different variables).
+// Variable names cannot be JavaScript reserved words (e.g., let, const, class, function, etc.
+ let Myvar = 10
+ let _myvar = 20
+ let $myvar = 30
+ let Myvar12 = 40
+ console.log(Myvar,_myvar,Myvar12,$myvar)
