1422cb8da415bccf974dba46c0c93e9d1e0f159d
gowtham jujaru <109477464+Gowthamjujaru@users.noreply.github.com>
2024-10-22 18:42:28 +0530

    Add files via upload

---
+++ Day8.js
@@ -0,0 +1,67 @@
+// // wjsp to allow user if his age is 18+ only he can drive
+/*
+let age = prompt("enter age")
+if(age>=18){
+    console.log("yes you can drive")
+}else{
+    console.log("no you should be 18y old")
+}
+    */
+// grade system
+/*
+let marks=prompt("enter your marks");
+if(marks>=90&&marks<=100){
+    console.log("you got A+")
+}if(marks>=80&&marks<=89){
+    console.log("you got A")
+}
+if(marks>=70&&marks<=79){
+    console.log("you got B+")
+}
+if(marks>=50&&marks<=69){
+    console.log("you got B")
+}
+if(marks>=35&&marks<=49){
+    console.log("you got C")
+}
+if(marks<=34){
+    console.log("you failed")
+}
+*/
+
+
+// wjsp to swap number
+/*
+let a = prompt("enter number a")
+let b = prompt("enter number b")
+console.log("before swap number a and b is :",a," ",b)
+
+let t=a;
+a=b;
+b=a;
+console.log("after swap number a and b is :",a," ",b)
+*/
+
+
+//  take a number from user and print from 1 to till user enterd 
+/*
+let a = prompt("enter a number")
+for(let i = 1;i<=a;i++){
+    console.log(i)
+}
+*/
+// take a number from user and print from 1 to till reverse user enterd 
+/*
+let a = prompt("enter a number")
+for(let i = a;i>=1;i--){
+    console.log(i)
+}
+*/
+/*
+let a = 1
+let n = prompt("enter a number")
+while(a<=n){
+    console.log(a)
+    a++
+}
+*/
---
+++ Functios_Day1.js
@@ -0,0 +1,16 @@
+function add(a,b){
+    return a+b;
+}
+function sub(a,b){
+    return a-b;
+}
+function mul(a,b){
+    return a*b;
+}
+function Interst(p,t,r){
+    return(p*t*r/100)
+}
+console.log(add(10,20))
+console.log(sub(10,20))
+console.log(mul(10,20))
+console.log(Interst(78968,2,8))
